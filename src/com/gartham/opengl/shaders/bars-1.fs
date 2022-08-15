b8854d3e82de3e12f43d05a8f2630edc5730c9e8
Gartham <gartham@dusttoash.org>
2022-08-15 03:56:35 -0500

    Update shaders

--- src/com/gartham/opengl/shaders/bars-1.fs
+++ src/com/gartham/opengl/shaders/bars-1.fs
@@ -7,6 +7,6 @@ uniform float time;
 
 void main()
 {
-	float val = round(sin((pos.x + 1) * 20));
+	float val = sin(time/2000f) * round(sin((pos.x + 1 + time / 1000f) * 20));
 	outputColor = vec4(val, val, val, 1);
 }
